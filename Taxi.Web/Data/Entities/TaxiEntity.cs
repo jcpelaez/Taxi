28d20f1420f8ca38eb89b10dda34180ecf81a2b0
JULIO CESAR PELAEZ SALAZAR <jcpelaezs@unadvirtual.edu.co>
2020-04-12 08:39:41 -0500

    creamos la entidad trip

---
+++ Taxi.Web/Data/Entities/TaxiEntity.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Taxi.Web.Data.Entities
+{
+    public class TaxiEntity
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Placa")]
+        [RegularExpression(@"^([A-Za-z]{3}\d{3})$", ErrorMessage = "El campo {0} debe iniciar con 3 letras y finalizar con 3 numeros")]
+        [StringLength(6, MinimumLength = 6, ErrorMessage = "El campo {0} debe tener {1} caracteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Plaque { get; set; }
+    }
+}
---
+++ Taxi.Web/Data/Entities/TripEntity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Taxi.Web.Data.Entities
+{
+    public class TripEntity
+    {
+        public int Id { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Start Date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd hh:mm}", ApplyFormatInEditMode = false)]
+        public DateTime StartDate { get; set; }
+
+        public DateTime StartDateLocal => StartDate.ToLocalTime();
+
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Start Date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd hh:mm}", ApplyFormatInEditMode = false)]
+        public DateTime? EndDate { get; set; }
+
+        public DateTime? EndDateLocal => EndDate?.ToLocalTime();
+
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener {1} caracteres.")]
+        public string Source { get; set; }
+
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener {1} caracteres.")]
+        public string Target { get; set; }
+
+        public float Qualification { get; set; }
+
+        public double SourceLatitude { get; set; }
+
+        public double SourceLongitude { get; set; }
+
+        public double TargetLatitude { get; set; }
+
+        public double TargetLongitude { get; set; }
+
+        public string Remarks { get; set; }
+
+        public TaxiEntity Taxi { get; set; }
+    }
+}
