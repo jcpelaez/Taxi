93ec7aeef50c09cc6afd491b139e42c9cf188689
JULIO CESAR PELAEZ SALAZAR <jcpelaezs@unadvirtual.edu.co>
2020-04-12 20:25:49 -0500

    menu prism

---
+++ Taxi.Prism/Taxi.Prism/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Taxi.Prism.ViewModels
+{
+    public class LoginPageViewModel : ViewModelBase
+    {
+        public LoginPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            Title = "Iniciar en la aplicación";
+        }
+    }
+}
---
+++ Taxi.Prism/Taxi.Prism/ViewModels/TaxiMasterDetailPageViewModel.cs
@@ -0,0 +1,72 @@
+﻿using Prism.Navigation;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Taxi.Common.Models;
+
+namespace Taxi.Prism.ViewModels
+{
+    public class TaxiMasterDetailPageViewModel : ViewModelBase
+    {
+        private readonly INavigationService _navigationService;
+
+        public TaxiMasterDetailPageViewModel(INavigationService navigationService) : base(navigationService)
+        {
+            _navigationService = navigationService;
+            LoadMenus();
+        }
+
+        public ObservableCollection<MenuItemViewModel> Menus { get; set; }
+
+        private void LoadMenus()
+        {
+            List<Menu> menus = new List<Menu>
+            {
+                new Menu
+                {
+                    Icon = "ic_airport_shuttle",
+                    PageName = "HomePage",
+                    Title = "Nuevo viaje"
+                },
+                new Menu
+                {
+                    Icon = "ic_local_taxi",
+                    PageName = "TaxiHistoryPage",
+                    Title = "Ver historia de taxi"
+                },
+                new Menu
+                {
+                    Icon = "ic_people",
+                    PageName = "GroupPage",
+                    Title = "Admin mi grupo de usuario"
+                },
+                new Menu
+                {
+                    Icon = "ic_account_circle",
+                    PageName = "ModifyUserPage",
+                    Title = "Modificar usuario"
+                },
+                new Menu
+                {
+                    Icon = "ic_report",
+                    PageName = "ReportPage",
+                    Title = "Reportar un incidente"
+                },
+                new Menu
+                {
+                    Icon = "ic_exit_to_app",
+                    PageName = "LoginPage",
+                    Title = "Loguearse"
+                }
+            };
+
+            Menus = new ObservableCollection<MenuItemViewModel>(
+                menus.Select(m => new MenuItemViewModel(_navigationService)
+                {
+                    Icon = m.Icon,
+                    PageName = m.PageName,
+                    Title = m.Title
+                }).ToList());
+        }
+    }
+}
